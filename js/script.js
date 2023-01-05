548ccdc258de8ca6531b2dd68259ae753ad0145c
justintate <justinpjtate1@gmail.com>
2023-01-05 17:21:04 +0000

    Added the change in turn functionality

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 const gridButtons = document.querySelectorAll('.grid-item');
-const turns = document.querySelectorAll('#scoreboard>p');
 
-gridButtons.forEach(value => {
-    value.addEventListener('click', function() {
-        value.innerText = document.querySelector('#current-go').innerText;
+const changeTurn = () => {
+    const turns = document.querySelectorAll('#scoreboard>p');
+    turns.forEach(turn => {
+        if(turn.id === 'current-go') {
+            turn.id = ''
+        } else if (turn.id === '') {
+            turn.id = 'current-go'
+        }
     })
-})
+}
 
-// turns.forEach(turn => {
-//     if(turn.id = 'current-go') {
-//         turn.id = '';
-//     } if(turn.id = '') {
-//         turn.id = 'current-go';
-//     }
-// })
+const gridEventListener = () => {
+    gridButtons.forEach(value => {
+        value.addEventListener('click', function() {
+            value.innerText = document.querySelector('#current-go').innerText;
+            changeTurn();
+        })
+    })
+}
+
+gridEventListener();

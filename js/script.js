5de234a048657f1db74b0bb8c3dc02065311ed86
justintate <justinpjtate1@gmail.com>
2023-01-06 11:36:57 +0000

    Added draw functionality to the game

--- js/script.js
+++ js/script.js
@@ -3,33 +3,6 @@ const gridButtonsArr = Array.prototype.slice.call(gridButtons);
 const turns = document.querySelectorAll('#scoreboard>p');
 
 const gameOver = () => {
-    if(gridButtonsArr.every(value => value.innerText !== '')) {
-        document.querySelector('#result').innerText = 'draw'
-    }
-}
-
-// const winningConditions = [
-//     [gridButtons[0].innerText, gridButtons[1].innerText, gridButtons[2].innerText],
-//     [gridButtons[3].innerText, gridButtons[4].innerText, gridButtons[5].innerText],
-//     [gridButtons[6].innerText, gridButtons[7].innerText, gridButtons[8].innerText],
-// ]
-
-// for (let i = 0; i < winningConditions.length; i++) {
-//     if(winningConditions[i][0] === winningConditions[i][1] === winningConditions[i][2] && winningConditions[i][0] !== '') {
-//         return document.querySelector('#result').innerText = 'winner'
-//     }
-// }
-
-// const gameOver2 = () => {
-//     gridButtons2 = document.querySelectorAll('.grid-item');
-//     for (let i = 0; i < gridButtons2.length; i++) {
-//         if (gridButtons2[i].innerText !== '' && gridButtons2[i].innerText === gridButtons2[i+1].innerText === gridButtons2[i+2].innerText) {
-//             document.querySelector('#result').innerText = 'winner'
-//         }
-//     }
-// }
-
-const gameOver3 = () => {
     const winningConditions = [
         [gridButtons[0].innerText, gridButtons[1].innerText, gridButtons[2].innerText],
         [gridButtons[3].innerText, gridButtons[4].innerText, gridButtons[5].innerText],
@@ -40,21 +13,17 @@ const gameOver3 = () => {
         [gridButtons[0].innerText, gridButtons[4].innerText, gridButtons[8].innerText],
         [gridButtons[2].innerText, gridButtons[4].innerText, gridButtons[6].innerText]
     ];
-    
-    winningConditions.forEach(value => {
-        if(value.every(v => v !== '' && v === value[0])) {
-            document.querySelector('#result').innerText = 'winner'
-        }
-    })
 
+    const winner = winningConditions.some(value => value.every(v => v !== '' && v === value[0]))
+    const boardNotFull = gridButtonsArr.some(value => value.innerText === '')
 
-    // if(winningConditions.forEach(value => {
-    //     value.every(v => v !== '' && v === value[0])
-    // }) === true) {
-    //     document.querySelector('#result').innerText = 'winner'
-    // } else if (gridButtonsArr.every(v => v.innerText !== '')) {
-    //     document.querySelector('#result').innerText = 'draw'
-    // }
+    if (boardNotFull === true && winner === true) {
+        document.querySelector('#result').innerText = 'winner'
+    } else if (boardNotFull === false && winner === true) {
+        document.querySelector('#result').innerText = 'winner'
+    } else if (boardNotFull === false) {
+        document.querySelector('#result').innerText = 'draw'
+    }
 }
 
 const changeTurn = () => {
@@ -70,10 +39,10 @@ const changeTurn = () => {
 const gridEventListener = () => {
     gridButtons.forEach(value => {
         value.addEventListener('click', function() {
-            if(value.innerText === '') {
+            if(value.innerText === '' && document.querySelector('#result').innerText === '') {
                 value.innerText = document.querySelector('#current-go').innerText;
                 changeTurn();
-                gameOver3();
+                gameOver();
             }
         })
     })
@@ -89,6 +58,7 @@ const restartGame = () => {
         });
         turns[0].id = 'current-go';
         turns[1].id = '';
+        document.querySelector('#result').innerText = '';
     });
 };
 
